ef0a1f1d7b6dcc4e61bb5c470907f7889876caf4
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2025-03-14 12:50:08 +0300

    Delete extension directory

--- extension/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("fetchNotes").addEventListener("click", async () => {  
-    const response = await fetch("http://localhost:5000/api/notes");  
-    const notes = await response.json();  
-    console.log("Processed notes:", notes);
-  });
